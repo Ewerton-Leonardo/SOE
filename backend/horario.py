418f0b6faa57708cab357991d02795f6a01f085e
Ewerton-Leonardo <33091155+Ewerton-Leonardo@users.noreply.github.com>
2018-12-08 17:44:24 -0300

    Remove pasta backend

--- backend/horario.py
+++
@@ -1,8 +0,0 @@
-class Horario:
-	def __init__(self, disciplina, professor, dia, hora, sala):
-		self.disciplina = disciplina
-		self.professor = professor
-		self.dia = dia
-		self.hora = hora
-		self.sala = sala
-
